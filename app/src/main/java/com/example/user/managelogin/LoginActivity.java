4556d23f04de6207adebf0c20a055841e23cbcac
KeithChien <windingram@gmail.com>
2016-12-26 14:14:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/managelogin/LoginActivity.java
@@ -0,0 +1,101 @@
+package com.example.user.managelogin;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText accountText;
+    private EditText passWordText;
+    private FirebaseAuth mfirebaseAuth;
+    private Button buttonLogin;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mfirebaseAuth = FirebaseAuth.getInstance();
+        accountText = (EditText) findViewById(R.id.editText);
+        passWordText = (EditText) findViewById(R.id.editText2);
+        Button buttonLogin = (Button) findViewById(R.id.button);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eMailLogin();
+                checkManager();
+
+            }
+        });
+
+
+    }
+
+    private void eMailLogin() {
+        final String email = "Manager@gmail.com";
+        final String password = "Manager@gmail.com";
+
+
+        mfirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                Log.d("TAG", "signInWithEmail:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w("TAG", "signInWithEmail:failed", task.getException());
+                }
+            }
+        });
+
+    }
+
+    private void checkManager() {
+        final String account = accountText.getText().toString();
+        final String acpassword = passWordText.getText().toString();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Admin");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println(dataSnapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+                if (account.equalsIgnoreCase(dataSnapshot.child("account").getValue().toString()) || acpassword.equalsIgnoreCase(dataSnapshot.child("password").getValue().toString())) {
+                    Intent intent = new Intent();
+                    intent.setClass(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
+
+
+
+
